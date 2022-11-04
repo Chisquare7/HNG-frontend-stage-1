d71b1f2af0c4599f92fb935337f23fbbd6cc9afd
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-11-04 03:08:04 +0100

    feat: Add font properties and margin bottom to the child selectors of header section of Contact page

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -7,58 +7,60 @@ const Contactpage = () => {
 		<div>
 			<div id="contact__overall">
 				<main id="contact__container">
-					<header className="contact__heading">
-						<h1 className="head__title">Contact Me</h1>
-						<p className="head__subtitle">
-							Hi there, contact me to ask me about anything you have in mind.
-						</p>
-					</header>
-					<form id="contact__form">
-						<div className="formname__flex">
-							<div className="input__name">
-								<label className="input__labels">First name</label>
-								<input
-									type="text"
-									id="first_name"
-									placeholder="Enter your first name"
-								/>
+					<div>
+						<header className="contact__heading">
+							<h1 className="head__title">Contact Me</h1>
+							<p className="head__subtitle">
+								Hi there, contact me to ask me about anything you have in mind.
+							</p>
+						</header>
+						<form id="contact__form">
+							<div className="formname__flex">
+								<div className="input__name">
+									<label className="input__labels">First name</label>
+									<input
+										type="text"
+										id="first_name"
+										placeholder="Enter your first name"
+									/>
+								</div>
+								<div className="input__name">
+									<label className="input__labels">Last name</label>
+									<input
+										type="text"
+										id="last_name"
+										placeholder="Enter your last name"
+									/>
+								</div>
 							</div>
-							<div className="input__name">
-								<label className="input__labels">Last name</label>
-								<input
-									type="text"
-									id="last_name"
-									placeholder="Enter your last name"
-								/>
+							<label className="input__labels">Email</label>
+							<input type="email" id="email" placeholder="yourname@email.com" />
+							<label className="input__labels">Message</label>
+							<textarea
+								id="message"
+								placeholder="Send me a message and I'll reply you as soon as possible..."
+							/>
+							<div className="input__tick">
+								<input type="checkbox" id="tick__box" />
+								<span className="tick__text">
+									You agree to providing your data to HNG who may contact you.
+								</span>
 							</div>
+							<input type="submit" id="btn__submit" value="Send message" />
+						</form>
+					</div>
+					<footer>
+						<hr />
+						<div id="footers">
+							<img
+								src={zuriInternship}
+								alt="Zuri Internship logo"
+								id="zuri__logo"
+							/>
+							<p id="footer__text">HNG Internship 9 Frontend Task</p>
+							<img src={I4G} alt="I4G logo" id="I4G__logo" />
 						</div>
-						<label className="input__labels">Email</label>
-						<input type="email" id="email" placeholder="yourname@email.com" />
-						<label className="input__labels">Message</label>
-						<textarea
-							id="message"
-							placeholder="Send me a message and I'll reply you as soon as possible..."
-						/>
-						<div className='input__tick'>
-							<input type="checkbox" id="tick__box" />
-							<span className='tick__text'>
-								You agree to providing your data to HNG who may contact you.
-							</span>
-						</div>
-						<input type="submit" id="btn__submit" value="Send message" />
-					</form>
-                    <footer>
-					    <hr />
-					    <div id="footers">
-						    <img
-							    src={zuriInternship}
-							    alt="Zuri Internship logo"
-							    id="zuri__logo"
-						    />
-						    <p id="footer__text">HNG Internship 9 Frontend Task</p>
-						    <img src={I4G} alt="I4G logo" id="I4G__logo" />
-					    </div>
-				    </footer>
+					</footer>
 				</main>
 				{/* <footer>
 					<hr />
